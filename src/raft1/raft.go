041358fef64ecfc3f45864c1ea6532d4cb0948c4
chriscco <2444628489c@gmail.com>
2025-04-07 11:37:14 +0800

    set up raft struct

--- src/raft1/raft.go
+++ src/raft1/raft.go
@@ -19,20 +19,59 @@ import (
 	"6.5840/tester1"
 )
 
+type LogEntry struct {
+	Command interface {}
+	Term int 
+}
 
 // A Go object implementing a single Raft peer.
 type Raft struct {
-	mu        sync.Mutex          // Lock to protect shared access to this peer's state
-	peers     []*labrpc.ClientEnd // RPC end points of all peers
-	persister *tester.Persister   // Object to hold this peer's persisted state
-	me        int                 // this peer's index into peers[]
-	dead      int32               // set by Kill()
+	mu        sync.Mutex          // 保护并发访问 Raft 共享状态
+	peers     []*labrpc.ClientEnd // 所有 Raft 节点的 RPC 客户端
+	persister *tester.Persister   // 用作持久化自己的状态，比如崩溃恢复时从磁盘读回
+	me        int                 // 本节点的 id（在 peers[] 中的位置）
+	dead      int32               // 是否已经被 Kill()，用来优雅退出
 
-	// Your data here (3A, 3B, 3C).
-	// Look at the paper's Figure 2 for a description of what
-	// state a Raft server must maintain.
+	// 持久化状态，必须保存到磁盘
+	currentTerm int // 代表当前的任期，每经历一次任期都将 term++, leader 的心跳会自带 term
+	votedFor int // 当前节点在当前任期内投给了哪个 ID 的节点，如果没有投则为 -1
+	log []LogEntry // 日志条目，用户输入的指令
 
+	// 易失状态，保存在内存
+	commitIndex int // 当前整个 Raft 集群已经被“大多数节点确认”的日志的最大索引
+	lastApplied int // 当前节点已经真正应用到本地状态机的最后一条日志索引
+
+	// 只由 leader 维护的状态
+	nextIndex []int // 对每个 follower，要发送的下一条日志的索引
+	matchIndex []int // 对每个 follower，被确认复制成功的最高日志索引
+
+	// 额外信息
+	state int // 当前节点角色，0：follower 1: candidate 2: leader
+	voteNum int // 当前 candidate 收到的投票数
+	stamp time.Time // 最近一次收到 leader 心跳/请求的时间，用于判断心跳超时
+	nextHeartBeat time.Time // 下一次应该发送心跳请求的时间
+
+	// 与服务应用层交互
+	applyChan chan raftapi.ApplyMsg // 用来通知应用层“可以应用一条日志了”（比如用户的 Put操作生效）
+
+	// 快照，用于日志压缩
+	snapshot []byte // 快照的二进制数据
+	lastIncludedTerm int // 快照最后一条日志的任期 
+	lastIncludedIndex int // 快照最后一条日志的索引
 }
+
+const (
+	Follower = 0 
+	Candidate = 1
+	Leader = 2
+	None = -1 
+)
+const (
+	HeartBeatInterval = 100
+	MaxElectionTime = 300 // 选举超时最大值，300ms
+    MinElectionTime = 150 // 选举超时最小值，150ms
+	ApplyInterval = 10 // 应用日志到状态机的检测频率
+)
 
 // return currentTerm and whether this server
 // believes it is the leader.
@@ -60,6 +99,9 @@ func (rf *Raft) persist() {
 	// e.Encode(rf.yyy)
 	// raftstate := w.Bytes()
 	// rf.persister.Save(raftstate, nil)
+}
+
+func (rf *Raft) ReadSnapshot(data []byte) {
 }
 
 
@@ -207,30 +249,39 @@ func (rf *Raft) ticker() {
 	}
 }
 
-// the service or tester wants to create a Raft server. the ports
-// of all the Raft servers (including this one) are in peers[]. this
-// server's port is peers[me]. all the servers' peers[] arrays
-// have the same order. persister is a place for this server to
-// save its persistent state, and also initially holds the most
-// recent saved state, if any. applyCh is a channel on which the
-// tester or service expects Raft to send ApplyMsg messages.
-// Make() must return quickly, so it should start goroutines
-// for any long-running work.
+// 创建一个 Raft 对象（new Raft）
+// 初始化必要的字段
+// 恢复崩溃前持久化的数据（日志、term、投票等）
+// 启动后台线程（goroutines）做心跳检测、选举、应用日志
+// 返回准备好的 *Raft 节点实例
 func Make(peers []*labrpc.ClientEnd, me int,
 	persister *tester.Persister, applyCh chan raftapi.ApplyMsg) raftapi.Raft {
 	rf := &Raft{}
 	rf.peers = peers
 	rf.persister = persister
 	rf.me = me
+	length := len(rf.peers)
+	rf.state = Follower
+	rf.voteNum = 0
+	rf.votedFor = None 
+	rf.currentTerm = 0
+	rf.matchIndex = make([]int, length) // 只会由 leader 维护
+	rf.stamp = time.Now()
 
-	// Your initialization code here (3A, 3B, 3C).
+	rf.log = append(rf.log, LogEntry{
+		Term: 0,
+		Command: nil,
+	})
+	rf.applyChan = applyCh
 
-	// initialize from state persisted before a crash
+	// 从持久化存储恢复状态
+	rf.ReadSnapshot(persister.ReadSnapshot())	
 	rf.readPersist(persister.ReadRaftState())
+	rf.nextIndex = make([]int, length)
 
 	// start ticker goroutine to start elections
 	go rf.ticker()
-
+	// go rf.applyToState() 
 
 	return rf
 }
